dd57c7094aa1688023bff54c3bc1086264be14ed
Uyooghene <uyooghene01@gmail.com>
2024-06-09 22:17:38 +0100

    best

--- app.js
+++ app.js
@@ -1,54 +1,3 @@
-// const formSearch = document.querySelector('#formsearch');
-// const inputtxt = document.querySelector('#inputtxt');
-// const butt = document.querySelector('#butt');
-// const showTitle = document.querySelector('#showTitle');
-// const container = document.querySelector('#container');
-
-// const submitfunc = async (e) => {
-//     container.style.display = 'block';
-//     showTitle.innerHTML = `You searched for shows containing : ${inputtxt.value}`
-//     e.preventDefault();
-//     const userInput = inputtxt.value;
-//     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${userInput}`)
-//     makeImage(res.data);
-//     console.log(res.data);
-// }
-
-// const makeImage = (shows) => {
-//     if (shows.length === 0) {
-//         const showTxt = document.createElement('h2');
-//         showTxt.innerHTML = 'This title does not exist on this site';
-//         container.appendChild(showTxt);
- 
-//     }
-//     for(let result of shows){
-//         if(result.show.image){
-//         const showImg = document.createElement('IMG');
-//         showImg.src = result.show.image.medium;
-//         container.appendChild(showImg);
-
-//     } else{
-//             const showText = document.createElement('h2');
-//             showText.innerHTML = result.show.name +' '+ 'image not available';
-//             container.appendChild(showText);
-
-//             }
-            
-//         }
-//     }
-//     inputtxt.addEventListener('input', () => {
-//         if (inputtxt.value === '') {
-//             clearSearch();
-//         }
-//     });
-
-//     const clearSearch = () => {
-//             container.style.display = 'none';
-//             showTitle.innerHTML = '';
-//     }
-   
-
-// formSearch.addEventListener('submit',submitfunc);
 
 const formSearch = document.querySelector('#formsearch');
 const inputtxt = document.querySelector('#inputtxt');
@@ -60,7 +9,7 @@ const submitfunc = async (e) => {
     const userInput = inputtxt.value;
     showTitle.innerHTML = `You searched for shows containing: ${userInput}`;
     const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${userInput}`);
-    clearResults(); // Clear previous results before displaying new ones
+    clearResults(); 
     makeImage(res.data);
     console.log(res.data);
 }
@@ -83,18 +32,18 @@ const makeImage = (shows) => {
             }
         }
     }
-    container.style.display = 'block'; // Show the container after adding results
+    container.style.display = 'block';
 }
 
 const clearResults = () => {
-    container.innerHTML = ''; // Clear all children of the container
-    showTitle.innerHTML = ''; // Clear the title
+    container.innerHTML = ''; 
+    showTitle.innerHTML = ''; 
 }
 
 inputtxt.addEventListener('input', () => {
     if (inputtxt.value === '') {
         clearResults();
-        container.style.display = 'none'; // Hide the container when input is cleared
+        container.style.display = 'none';
     }
 });
 
