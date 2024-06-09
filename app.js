cb60ab442f3d661d1e31c503c6be868f4e89426e
Uyooghene <uyooghene01@gmail.com>
2024-06-09 22:06:37 +0100

    good

--- app.js
+++ app.js
@@ -1,15 +1,50 @@
 const formSearch = document.querySelector('#formsearch');
 const inputtxt = document.querySelector('#inputtxt');
 const butt = document.querySelector('#butt');
+const showTitle = document.querySelector('#showTitle');
+const container = document.querySelector('#container');
 
 const submitfunc = async (e) => {
+    showTitle.innerHTML = `You searched for shows containing : ${inputtxt.value}`
     e.preventDefault();
     const userInput = inputtxt.value;
-   const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${userInput}`)
-   console.log(res.data[1].show.image.medium);
-   const showImg = document.createElement('IMG');
-  showImg.src = res.data[1].show.image.medium;
-   document.body.appendChild(showImg)
-
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${userInput}`)
+    makeImage(res.data);
+    console.log(res.data);
 }
+
+const makeImage = (shows) => {
+    if (shows.length === 0) {
+        const showTxt = document.createElement('h2');
+        showTxt.innerHTML = 'This title does not exist on this site';
+        container.appendChild(showTxt);
+ 
+    }
+    for(let result of shows){
+        if(result.show.image){
+        const showImg = document.createElement('IMG');
+        showImg.src = result.show.image.medium;
+        container.appendChild(showImg);
+
+    } else{
+            const showText = document.createElement('h2');
+            showText.innerHTML = result.show.name +' '+ 'image not available';
+            container.appendChild(showText);
+
+            }
+            
+        }
+    }
+    inputtxt.addEventListener('input', () => {
+        if (inputtxt.value === '') {
+            clearSearch();
+        }
+    });
+
+    const clearSearch = () => {
+            container.style.display = 'none';
+            showTitle.innerHTML = '';
+    }
+   
+
 formSearch.addEventListener('submit',submitfunc);
